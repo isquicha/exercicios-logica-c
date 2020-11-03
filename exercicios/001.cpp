61765a9ce9acae084642dd32763746badd369ab1
Andre Vicente <isquicha@gmail.com>
2020-11-03 01:46:49 -0300

    Add exercicio 1

---
+++ exercicios/001.cpp
@@ -0,0 +1,12 @@
+/*
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <cmath>
+#include <cstring>
+int main()
+{
+    printf("Alo mundo");
+    return 0;
+}
